f5d1e3595aaee073fadbcd84784262fb76d1bb58
marajul-enosis <marajul.islam@enosisbd.com>
2023-05-31 00:25:41 +0600

    [Added]: prime vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import "primeflex/primeflex.css";
+import PrimeVue from 'primevue/config';
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import { createApp } from 'vue';
+import App from './App.vue';
+import "./assets/style.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const app = createApp(App)
+app.use(PrimeVue);
 
-createApp(App).mount('#app')
+
+app.mount('#app')
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <p>Home page content goes here.</p>
+    </div>
+    <div class="card flex justify-content-center">
+        <Button label="Check" icon="pi pi-check" />
+    </div>
+</template>
+
+<script setup>
+import Button from "primevue/button";
+</script>
